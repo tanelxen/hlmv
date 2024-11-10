38bcab1c60b21ac26a7d1ad5f61b472e3c993e89
Tanelxen <tanelxen@gmail.com>
2024-11-10 18:20:45 +0300

    New parser

---
+++ src/Renderer.cpp
@@ -0,0 +1,8 @@
+//
+//  Renderer.cpp
+//  hlmv
+//
+//  Created by Fedor Artemenkov on 11.11.24.
+//
+
+#include "Renderer.hpp"
---
+++ src/Renderer.h
@@ -0,0 +1,13 @@
+//
+//  Renderer.hpp
+//  hlmv
+//
+//  Created by Fedor Artemenkov on 11.11.24.
+//
+
+#ifndef Renderer_hpp
+#define Renderer_hpp
+
+#include <stdio.h>
+
+#endif /* Renderer_hpp */
